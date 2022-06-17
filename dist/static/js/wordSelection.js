c8e1cef125690eabc2c8be9ad4f46ceb9d68b979
Daniel Puente <dpr1005@alu.ubu.es>
2022-06-18 01:26:43 +0200

    [Frontend] JS support for select size of words available

--- dist/static/js/wordSelection.js
+++ dist/static/js/wordSelection.js
@@ -1,16 +1,30 @@
 const invoke = window.__TAURI__.invoke;
 
+function isNumeric(val) {
+    return /^-?\d+$/.test(val);
+}
+
 function addOptionsToSelect(selectId, options) {
     let select = document.getElementById(selectId);
+    let selectLength = select.length;
+    for (let i = 0; i < selectLength; i++) {
+        select.removeChild(select.lastChild);
+    }
     if (!options.includes('random')) {
         options.push('random');
     }
-    options.sort();
+    options.sort((a, b) => ((typeof b === "number") - (typeof a === "number")) || (a > b ? 1 : -1));
+    console.log(options);
 
     for(let i = 0; i < options.length; i++) {
         let option = document.createElement("option");
-        option.value = options[i].toLowerCase();
-        option.innerHTML = options[i].charAt(0).toUpperCase() + options[i].slice(1);
+        if (!isNumeric(options[i])) {
+            option.value = options[i].toLowerCase();
+            option.innerHTML = options[i].charAt(0).toUpperCase() + options[i].slice(1);
+        } else {
+            option.value = options[i];
+            option.innerHTML = options[i];
+        }
         select.appendChild(option);
     }
 }
@@ -33,4 +47,17 @@ function findWordGroups() {
         .catch(e => {
             console.log(e);
         });
+}
+
+function findWordsLength() {
+    invoke('find_lengths', {
+        wordType: document.getElementById('wordTypeSelect').value,
+        group: document.getElementById('wordGroupSelect').value
+    })
+    .then(function (result) {
+        addOptionsToSelect('wordLengthSelect', result);
+    })
+    .catch(e => {
+        console.log(e);
+    });
 }
