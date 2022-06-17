c873d04003611a205f11db10bf18d97a7844e44a
Daniel Puente <dpr1005@alu.ubu.es>
2022-06-18 01:28:04 +0200

    [Backend] Updated DB and retrieve size of words

--- src-tauri/src/database.rs
+++ src-tauri/src/database.rs
@@ -1,14 +1,15 @@
+use std::string;
 
-
-
-use rusqlite::{Connection, Result};
+use rusqlite::{Connection, Result, types, Statement, Rows};
+use serde::de::value::Error;
 
 use crate::structs::Word;
 
-pub fn insert_record(conn: &mut Connection, record: Word) -> Result<()> {
-    
+#[tauri::command]
+pub fn insert_record(record: Word) -> Result<()> {
     let size = record.word.len() as i32;
-    
+    let conn: Connection = Connection::open("database.sqlite").unwrap();
+   
     conn.execute(
         "INSERT INTO words (word, language, type, group_, size) VALUES (?1, ?2, ?3, ?4, ?5)",
         &[&record.word, &record.language, &record.type_, &record.group, &size.to_string()],
@@ -17,11 +18,76 @@ pub fn insert_record(conn: &mut Connection, record: Word) -> Result<()> {
     Ok(())
 }
 
-pub fn remove_record(conn: &mut Connection) -> Result<()> {
+#[tauri::command]
+pub fn remove_record() -> Result<()> {
+    let conn: Connection = Connection::open("database.sqlite").unwrap();
+
     conn.execute(
         "DELETE FROM words WHERE id = $1",
         &[&1],
     )?;
 
     Ok(())
+}
+
+pub fn unique_types() -> Result<Vec<String>> {
+    let conn: Connection = Connection::open("database.sqlite").unwrap();
+
+    let mut stmt = conn.prepare("SELECT DISTINCT type FROM words").unwrap();
+    let mut rows = stmt.query([])?;
+
+    let mut types = Vec::new();
+    while let Some(row) = rows.next()? {
+        types.push(row.get(0)?);
+    }
+
+    Ok(types)
+}
+
+pub fn unique_groups() -> Result<Vec<String>> {
+    let conn: Connection = Connection::open("database.sqlite").unwrap();
+
+    let mut stmt = conn.prepare("SELECT DISTINCT group_ FROM words").unwrap();
+    let mut rows = stmt.query([])?;
+
+    let mut groups = Vec::new();
+    while let Some(row) = rows.next()? {
+        groups.push(row.get(0)?);
+    }
+
+    Ok(groups)
+}
+
+pub fn unique_lengths(type_: String, group_: String) -> Result<Vec<i32>> {
+    let conn: Connection = Connection::open("database.sqlite").unwrap();
+    let mut query = "";
+    let mut stmt;
+    let mut rows;
+
+    if type_ == "random" && group_ != "random" {
+        query = "SELECT DISTINCT size FROM words WHERE group_ = $1";
+        stmt = conn.prepare(query).unwrap();
+        rows = stmt.query(&[&group_])?;
+    }
+    else if group_ == "random" && type_ != "random" {
+        query = "SELECT DISTINCT size FROM words WHERE type = $1";
+        stmt = conn.prepare(query).unwrap();
+        rows = stmt.query(&[&type_])?;
+    }
+    else if type_ != "random" && group_ != "random" {
+        query = "SELECT DISTINCT size FROM words WHERE type = $1 AND group_ = $2";
+        stmt = conn.prepare(query).unwrap();
+        rows = stmt.query(&[&type_, &group_])?;
+    } else {
+        query = "SELECT DISTINCT size FROM words";
+        stmt = conn.prepare(query).unwrap();
+        rows = stmt.query([])?;
+    }
+
+    let mut lengths = Vec::new();
+    while let Some(row) = rows.next()? {
+        lengths.push(row.get(0)?);
+    }
+
+    Ok(lengths)
 }
--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -5,33 +5,33 @@
 
 pub mod database;
 pub mod structs;
-use database::insert_record;
+use database::{insert_record, remove_record, unique_types, unique_groups, unique_lengths};
 use structs::Word;
 
-use rusqlite::{Connection, Result};
+use rusqlite::{Connection, Result, types};
 
 #[tauri::command]
 fn kill_app() {
     println!("Killing app...");
     panic!("Killing app...");
 }
-
 fn main() -> Result<()> {
-    let mut conn_mut = Connection::open("database.sqlite")?;
-    let conn = Connection::open("database.sqlite")?;
+    let conn_mut = Connection::open("database.sqlite")?;
+    let conn: Connection = Connection::open("database.sqlite").unwrap();
+    //conn = Some(Connection::open("database.sqlite")?);
 
-    let mut stmt = conn.prepare("SELECT * FROM words")?;
+    // let mut stmt = conn.prepare("SELECT * FROM words")?;
 
-    let rows = stmt.query_map([], |row| {
-        Ok(Word {
-            id: row.get(0)?,
-            word: row.get(1)?,
-            language: row.get(2)?,
-            type_: row.get(3)?,
-            group: row.get(4)?,
-            size: row.get(5)?,
-        })
-    })?;
+    // let rows = stmt.query_map([], |row| {
+    //     Ok(Word {
+    //         id: row.get(0)?,
+    //         word: row.get(1)?,
+    //         language: row.get(2)?,
+    //         type_: row.get(3)?,
+    //         group: row.get(4)?,
+    //         size: row.get(5)?,
+    //     })
+    // })?;
 
     // insert_record(&mut conn_mut,  Word {
     //     id: 1000,
@@ -42,17 +42,35 @@ fn main() -> Result<()> {
     //     size: 5,
     // })?;
 
-    rows.for_each(|row| {
-        println!("{:?}", row.unwrap());
-    });
+    // rows.for_each(|row| {
+    //     println!("{:?}", row.unwrap());
+    // });
 
-
+    
     // https://rust-lang-nursery.github.io/rust-cookbook/database/sqlite.html#sqlite
 
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![kill_app])
+        .invoke_handler(tauri::generate_handler![kill_app, find_uniques, find_groups, find_lengths])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 
         Ok(())
+}
+
+#[tauri::command]
+fn find_uniques() -> Vec<String>{
+    println!("{:?}", unique_types().unwrap());
+    return unique_types().unwrap();
+}
+
+#[tauri::command]
+fn find_groups () -> Vec<String>{
+    println!("{:?}", unique_groups().unwrap());
+    return unique_groups().unwrap();
+}
+
+#[tauri::command]
+fn find_lengths(word_type: String, group: String) -> Vec<i32>{
+    println!("{:?}", unique_lengths(word_type.clone(), group.clone()).unwrap());
+    return unique_lengths(word_type, group).unwrap();
 }
