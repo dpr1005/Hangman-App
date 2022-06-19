754a0b121885638d5f917e2597e001eba63a594d
Daniel Puente <dpr1005@alu.ubu.es>
2022-06-19 23:22:51 +0200

    [Frontend] Invoke method to save words into DB

--- dist/static/js/editDB.js
+++ dist/static/js/editDB.js
@@ -1,16 +1,25 @@
 const invoke = window.__TAURI__.invoke;
 
+let typesDB = [];
+let languagesDB = [];
+
 function addWordToDB(word, lang, type_, group) {
+
+    lang = lang.toUpperCase();
+    word = word.toLowerCase();
+    type_ = type_.toLowerCase();
+    group = group.toLowerCase();
+
     invoke('add_word', {
         word: word,
         language: lang,
         type: type_,
         group: group
     })
-    .then((result) => {
-        alert('Word added to DB');
-    })
-    .error((err) => {
-        alert('Error adding word to DB');
-    });
+        .then((result) => {
+            alert('Word added to DB');
+        })
+        .error((err) => {
+            alert('Error adding word to DB');
+        });
 }
